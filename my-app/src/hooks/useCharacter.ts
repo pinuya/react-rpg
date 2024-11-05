4ed19025f6496287d324f3745b8d6d2e66c44df9
Tifany <tifsnunes@gmail.com>
2024-11-05 17:46:39 -0300

    if check to not leave the map

--- my-app/src/hooks/useCharacter.ts
+++ my-app/src/hooks/useCharacter.ts
@@ -1,49 +1,58 @@
-import { useState } from "react"
-import { CharacterSides } from '../types/CharacterSides'
+import { useState } from "react";
+import { CharacterSides } from "../types/CharacterSides";
+import { mapSpots } from "../data/mapSpots";
 
 export const useCharacter = () => {
-    const [pos, setPos] = useState({x: 3, y: 5});
-    const [side, setSide] = useState<CharacterSides>('down');
+  const [pos, setPos] = useState({ x: 3, y: 5 });
+  const [side, setSide] = useState<CharacterSides>("down");
 
-    const moveLeft = () => {
-        setPos(pos => ({
-            x: pos.x - 1,
-            y: pos.y 
-        }))
-        setSide('left')
-    }
+  const moveLeft = () => {
+    setPos((pos) => ({
+      x: canMove(pos.x - 1, pos.y) ? pos.x - 1 : pos.x,
+      y: pos.y,
+    }));
 
-    const moveRight = () => {
-        setPos(pos => ({
-            x: pos.x + 1,
-            y: pos.y 
-        }))
-        setSide('right')
-    }
+    setSide("left");
+  };
 
-    const moveDown = () => {
-        setPos(pos => ({
-            x: pos.x,
-            y: pos.y + 1 
-        }))
-        setSide('down')
-    }
+  const moveRight = () => {
+    setPos((pos) => ({
+      x: canMove(pos.x + 1, pos.y) ? pos.x + 1 : pos.x,
+      y: pos.y,
+    }));
 
-    const moveUp = () => {
-        setPos(pos => ({
-            x: pos.x,
-            y: pos.y - 1
-        }))
-        setSide('down')
-    }
+    setSide("right");
+  };
 
-    return{
-        x: pos.x,
-        y: pos.y,
-        side,
-        moveLeft,
-        moveRight,
-        moveDown,
-        moveUp
-    };
-}
+  const moveDown = () => {
+    setPos((pos) => ({
+      x: pos.x,
+      y: canMove(pos.x, pos.y + 1) ? pos.y + 1 : pos.y,
+    }));
+    setSide("down");
+  };
+
+  const moveUp = () => {
+    setPos((pos) => ({
+      x: pos.x,
+      y: canMove(pos.x, pos.y - 1) ? pos.y - 1 : pos.y,
+    }));
+    setSide("down");
+  };
+
+  const canMove = (x: number, y: number) => {
+    if (x < 0 || y < 0) return false;
+
+    return true;
+  };
+
+  return {
+    x: pos.x,
+    y: pos.y,
+    side,
+    moveLeft,
+    moveRight,
+    moveDown,
+    moveUp,
+  };
+};
